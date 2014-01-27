0dd5642d393c7805cc2c5ae129a5c13d9cbac0c4
saml <skynare@gmail.com>
2014-01-27 12:03:30 -0500

    asdf

--- doitbro.py
+++ doitbro.py
@@ -1,30 +1,37 @@
-'do it bro'
+'''do it bro
+
+replSet=rs0
+oplogSize=128
+
+'''
 
 import argparse
 import traceback
 import time
+import logging
 
 import pymongo
+
+logging.basicConfig(format='%(asctime)s %(levelname)s %(filename)s:%(lineno)d | %(message)s', level=logging.INFO)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--dburl', default='mongodb://localhost:27017', help='mongodb url without db part [%(default)s]')
     args = parser.parse_args()
     client = pymongo.MongoClient(args.dburl)
+    logging.info('connected to %s', args.dburl)
     db = client.local
     collection = db['oplog.rs']
-    cursor = collection.find(tailable=True)
+    ts = collection.find().sort('$natural', -1)[0]['ts']
     while True:
         try:
-            if cursor.alive:
-                x = cursor.next()
-                print(x)
-            else: 
-                print(cursor)
-        except KeyboardInterrupt:
-            break
-        except StopIteration:
+            query = {'ts': {'$gt': ts}}
+            logging.debug(query)
+            cursor = collection.find(query, tailable=True)
+            for op in cursor:
+                ts = op['ts']
+                logging.info(op)
             time.sleep(1)
-        except:
-            traceback.print_exc()
-
+        except pymongo.errors.AutoReconnect:
+            logging.exception('cannot reconnect')
+            time.sleep(1)
